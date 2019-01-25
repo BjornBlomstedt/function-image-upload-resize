1363304710a63f0d442759663f12b8d1f6dec3d6
BjornBlomstedt <46929346+BjornBlomstedt@users.noreply.github.com>
2019-01-25 10:32:17 +0100

    Update Thumbnail.cs

--- ImageFunctions/Thumbnail.cs
+++ ImageFunctions/Thumbnail.cs
@@ -88,7 +88,7 @@ namespace ImageFunctions
                     if (encoder != null)
                     {
                         var thumbnailWidth = 100;
-                        var thumbContainerName = "thumbnails";
+                        var thumbContainerName = "myContainerName";
                         var storageAccount = CloudStorageAccount.Parse(BLOB_STORAGE_CONNECTION_STRING);
                         var blobClient = storageAccount.CreateCloudBlobClient();
                         var container = blobClient.GetContainerReference(thumbContainerName);
